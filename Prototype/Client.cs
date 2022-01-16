f15111182a8a9dbd9002f6fe02fcd0559be6d5ad
rotemdan267 <rotemdan267@gmail.com>
2022-01-16 09:11:22 +0200

    Prototype

---
+++ Prototype/Client.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    interface IProto<T>
+    {
+        T Clone();
+    }
+    internal class Client : IProto<Client>
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(int iD, string name, string address)
+        {
+            ID = iD;
+            Name = name;
+            Address = address;
+        }
+
+        public Client Clone()
+        {
+            return new Client(this.ID, this.Name, this.Address);
+        }
+        public override string ToString()
+        {
+            return $"ID: {ID} \nName: {Name} \n Address: {Address}";
+        }
+    }
+    class Bill : IProto<Bill>
+    {
+        public Client Client { get; set; }
+        public float PricePaid { get; set; }
+        public float VAT { get; set; }
+        public DateTime DateOfDeal { get; set; }
+
+        public Bill(Client client, float pricePaid, float vAT, DateTime dateOfDeal)
+        {
+            Client = client;
+            PricePaid = pricePaid;
+            VAT = vAT;
+            DateOfDeal = dateOfDeal;
+        }
+
+        public Bill Clone()
+        {
+            return new Bill(this.Client.Clone(), this.PricePaid, this.VAT, this.DateOfDeal);
+        }
+
+        public override string ToString()
+        {
+            return $"Client: {Client.ToString()} " +
+                $"\n PricePaid: {PricePaid} " +
+                $"\nVAT: {VAT} \nDate: {DateOfDeal.ToString()}";
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,34 @@
+﻿using Prototype;
+using System.Text.Json;
+
+Client client = new Client(1, "abc", "qwe 3, holon");
+Bill bill = new Bill(client, 109.99f, 18.69f, DateTime.Now);
+
+//Bill bill2 = bill.Clone();
+Bill bill2 = DeepCopy(bill);
+
+Console.WriteLine(bill);
+Console.WriteLine(bill2);
+
+client.ID = 3;
+client.Name = "cdf";
+client.Address = "jhjh 3, hadera";
+
+bill.Client = client;
+bill.PricePaid = 20f;
+bill.VAT = 3.4f;
+bill.DateOfDeal = DateTime.Now.AddDays(17);
+
+
+Console.WriteLine("------------------------------------------------------------");
+
+Console.WriteLine(bill);
+Console.WriteLine(bill2);
+
+
+static T DeepCopy<T>(T self)
+{
+    var json = JsonSerializer.Serialize(self);
+    T obj = JsonSerializer.Deserialize<T>(json);
+    return obj;
+}
