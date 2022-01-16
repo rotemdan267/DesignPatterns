ef1c76b2ab63fb479716b9e3a65d4a6eae8bd719
rotemdan267 <rotemdan267@gmail.com>
2022-01-16 06:01:03 +0200

    Factory

---
+++ Factory/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    internal class MyClass
+    {
+        private MyClass()
+        {
+        }
+        public static MyClass Create()
+        {
+            return new MyClass();
+        }
+    }
+}
